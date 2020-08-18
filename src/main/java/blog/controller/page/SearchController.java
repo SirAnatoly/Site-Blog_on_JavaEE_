7f37b761d4bacee77858164b9959e3bd7c81ce28
MrAnatoliy <kovalv97@gmail.com>
2020-08-18 20:37:07 +0300

    Implementation of search articles in blog

--- src/main/java/blog/controller/page/SearchController.java
+++ src/main/java/blog/controller/page/SearchController.java
@@ -1,6 +1,10 @@
 package blog.controller.page;
 
+import blog.Constants;
 import blog.controller.AbstractController;
+import blog.entity.Article;
+import blog.model.Items;
+import org.apache.commons.lang3.StringUtils;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -10,8 +14,18 @@ import java.io.IOException;
 
 @WebServlet({"/search"})
 public class SearchController extends AbstractController {
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        forward_to_page("search.jsp",req,resp);
+        String query = req.getParameter("query");
+        if (StringUtils.isNotBlank(query)) {
+            Items<Article> items = getBusinessService().listArticlesBySearchQuery(query, 0, Constants.LIMIT_ARTICLES_PER_PAGE);
+            req.setAttribute("list", items.getItems());
+            req.setAttribute("count", items.getCount());
+            req.setAttribute("searchQuery", query);
+            forward_to_page("search.jsp", req, resp);
+        } else {
+            resp.sendRedirect("/Site_Blog_on_JavaEE_war/news");
+        }
     }
 }
